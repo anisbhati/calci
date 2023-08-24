6fd67b33ce527cebdbb9805540f0e2ba60ea9c54
anisbhati <54569086+anisbhati@users.noreply.github.com>
2023-08-24 21:14:04 +0530

    dev commit

--- src/main/java/com/example/demo/controller/CalciApiController.java
+++ src/main/java/com/example/demo/controller/CalciApiController.java
@@ -7,4 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalciApiController {
    //dev
+
+   // local prod 
+	@GetMapping("/getDetails")
+	public String getDetails() {
+		//remote add
+		return null;
+	}
 }
