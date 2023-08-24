b697ad5def259551d02935e1bd7480b73d9a1d76
anisbhati <54569086+anisbhati@users.noreply.github.com>
2023-08-24 21:05:53 +0530

    prod local commit

--- src/main/java/com/example/demo/controller/CalciApiController.java
+++ src/main/java/com/example/demo/controller/CalciApiController.java
@@ -1,4 +1,4 @@
-rpackage com.example.demo.controller;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,22 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalciApiController {
 
-     //server prod comment
-
-    
-	@GetMapping("/getTest")
-	public String getTest() {
-		return "running.........";
+   // local prod 
+	@GetMapping("/getDetails")
+	public String getDetails() {
+		return null;
 	}
-	
-
-	@GetMapping("/sub/{a}/{b}")
-	public String sub(@PathVariable int a,@PathVariable int b) {
-		return "sub is "+(a-b);
-	}
-	@GetMapping("/add/{a}/{b}")
-	public String add(@PathVariable int a,@PathVariable int b) {
-		return "add is "+(a+b);
-	}
-
 }
