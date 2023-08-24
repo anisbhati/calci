5e359158b1362ca535a176b3dbeacf77ddd9f7fe
anisbhati <54569086+anisbhati@users.noreply.github.com>
2023-08-24 21:02:13 +0530

    Update CalciApiController.java

--- src/main/java/com/example/demo/controller/CalciApiController.java
+++ src/main/java/com/example/demo/controller/CalciApiController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+rpackage com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalciApiController {
 
-     //prod comment
+     //server prod comment
 
     
 	@GetMapping("/getTest")
